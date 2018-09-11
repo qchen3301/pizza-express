7dd26e885ef6b095086ae6fec4934461f1c183c9
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-09-11 17:51:41 -0400

    completed GET routes

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ const app     = express();
 // assigning 3000 as our port
 const PORT    =  process.env.PORT || 3000;
 
+app.get('/', (req,res) => {
+	res.send('Welcome to Pizza Express!')
+})
+
+app.get('/topping/:type', (req,res, next) => {
+	res.send(`${req.params.type} pizza! Good choice.`)
+})
+
+app.get('/order/:amount/:size', (req, res, next) => {
+	res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in one minute!`)
+})
+
+
+
+
+
 // tells the server to listen for requests on port 3000
 app.listen(PORT, function(){
   console.log("==========================")
